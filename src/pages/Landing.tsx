d60f87d23696df283cdefee2bcc905134789dce8
sajalpaudyal <paudyalsajal@gmail.com>
2025-12-17 18:09:05 +0100

    addition: roommate finder section on homepage

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import OurService from "./Landing/OurService";
 import Hero from "./Landing/Hero";
+import RoommateFinder from "./Landing/RoommateFinder";
 
 const Landing: React.FC = () => {
   return (
     <>
       <Hero />
       <OurService />
+      <RoommateFinder/>
     </>
   );
 };
---
+++ src/pages/Landing/RoommateFinder.tsx
@@ -0,0 +1,15 @@
+const RoommateFinder = () => {
+  return (
+    <div className="bg-gray-900 py-16 text-white overflow-hidden relative">
+        <div className="absolute top-0 right-0 -mr-20 -mt-20 w-64 h-64 bg-indigo-600 rounded-full opacity-20 blur-3xl"></div>
+        <div className="absolute bottom-0 left-0 -ml-20 -mb-20 w-80 h-80 bg-purple-600 rounded-full opacity-20 blur-3xl"></div>
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default RoommateFinder
